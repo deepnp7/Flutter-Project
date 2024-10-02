7435a10c4caf99d02a9de5727b310b0d955c300b
Deepnarayan Patra <deepnp7@gmail.com>
2024-10-03 02:07:09 +0530

    Day 20 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,6 +3,7 @@
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
+import 'package:velocity_x/velocity_x.dart';
 // import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
@@ -35,7 +36,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
+        color: context.canvasColor,
         child: SingleChildScrollView(
           child: Form(
             key: _formkey,
